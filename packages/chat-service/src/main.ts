3144e1bf91d1fbc0158c8f8fcc511a3e2be405bd
gregormammoth <grigory.mamontov@frumatic.com>
2025-06-25 21:23:03 +0200

    fix: chat socket

--- packages/chat-service/src/main.ts
+++ packages/chat-service/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
+  app.useWebSocketAdapter(new IoAdapter(app));
+
   app.enableCors({
     origin: process.env.CLIENT_URL || 'http://localhost:3000',
     methods: ['GET', 'POST'],
